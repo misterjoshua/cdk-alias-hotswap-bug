c8a322b4828ad6e6bd6ed3836483a66e4ca0d915
Josh Kellendonk <joshkellendonk@gmail.com>
2022-10-07 11:12:40 -0600

    mvb

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,35 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-
-export class MyStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps = {}) {
-    super(scope, id, props);
-
-    // define resources here...
-  }
-}
-
-// for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
+import { App, aws_apigateway, aws_lambda, Stack } from 'aws-cdk-lib';
 
 const app = new App();
 
-new MyStack(app, 'cdk-alias-hotswap-bug-dev', { env: devEnv });
-// new MyStack(app, 'cdk-alias-hotswap-bug-prod', { env: prodEnv });
+const stack = new Stack(app, 'cdk-alias-hotswap-bug', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
+
+const handler = new aws_lambda.Function(stack, 'Handler', {
+  code: aws_lambda.Code.fromInline([
+    'def handler(event, ctx):',
+    '  return { "statusCode": 200, "body": "{}" }',
+    // Introduce randomness for convenience so you can hit save on this file
+    // to redeploy and trigger the bug.
+    `  #${Math.random()}`,
+    '',
+  ].join('\n')),
+  runtime: aws_lambda.Runtime.PYTHON_3_9,
+  handler: 'index.handler',
+});
+
+// Add provisioned concurrency
+const handlerAlias = handler.addAlias('abcd', {
+  provisionedConcurrentExecutions: 3,
+});
+
+// Use the provisioned concurrency as the rest api handler
+new aws_apigateway.LambdaRestApi(stack, 'Api', {
+  handler: handlerAlias,
+});
 
 app.synth();
